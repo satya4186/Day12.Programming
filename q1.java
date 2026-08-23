39d58896e052723fe45d076217b31b3b8e93ba86
satya4186 <satyapaidimukkala@gmail.com>
2026-08-23 10:16:34 +0530

    q1.java

---
+++ q1.java
@@ -0,0 +1,15 @@
+class q1{
+
+    static void print(int n) {
+        if (n == 0) {
+            return;
+        }
+        System.out.println(n);
+        print(n - 1);
+    }
+
+    public static void main(String[] args) {
+
+        print(5);
+    }
+}
